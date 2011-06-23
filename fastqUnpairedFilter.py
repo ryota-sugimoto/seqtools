9533e1625c5e34dd939eb73a73312d998fb1a49c
ryota sugimoto <ryota.sugimoto@gmail.com>
2011-06-23 10:13:36 +0900

    fasqUnpairedFilter.py is using stream now

--- fastqUnpairedFilter.py
+++ fastqUnpairedFilter.py
@@ -2,43 +2,51 @@
 
 import sys
 
-def getID(s):
-	temp = s.split('#')[0].split(':')[-4:]
+def getID(line):
+	#
+	#This function read a ID from the ID line in sequence data.
+	#You may have to edit this function for your specified fastq format.
+	#
+	temp = line.split('#')[0].split(':')[-4:]
 	return temp[0] + ':' + temp[1] + ':' + temp[2] + ':' + temp[3]
 
-def getIDlist(filelines):
+def getIDlist(fastqfile):
+	fastqfile.seek(0)
 	res = []
 	count = 0
-	for line in filelines:
+	for line in fastqfile:
 		if count % 4 == 0:
 			res.append(getID(line))
 		count = count + 1
 	return res
 
-def fileFilter(srcfilelines, fileIDlist, passSet):
-	srciter = iter(srcfilelines)
-	res = []
-	for ID in fileIDlist:
+def fileFilter(in_fastqfile, in_fileIDlist, out_fastqfile, passSet):
+	in_fastqfile.seek(0)
+	out_fastqfile.seek(0)
+	for ID in in_fileIDlist:
 		if ID in passSet:
-			for i in xrange(4): res.append(srciter.next())
+			for i in xrange(4):
+				out_fastqfile.write(in_fastqfile.next())
 		else:
-			for i in xrange(4): srciter.next()
-	return res
+			for i in xrange(4): in_fastqfile.next()
 
-def filterMain(src1, src2, tar1, tar2):
-	srcfile1list = open(src1,'r').readlines()
-	srcfile2list = open(src2,'r').readlines()
+def filterMain(in_1, in_2, out_1, out_2):
+	in_file1 = open(in_1,'r')
+	in_file2 = open(in_2,'r')
 
-	file1IDlist = getIDlist(srcfile1list)
-	file2IDlist = getIDlist(srcfile2list)
+	file1IDlist = getIDlist(in_file1)
+	file2IDlist = getIDlist(in_file2)
 
 	passSet = set(file1IDlist) & set(file2IDlist)
 	
-	tarfile1list = fileFilter(srcfile1list, file1IDlist, passSet)
-	tarfile2list = fileFilter(srcfile2list, file2IDlist, passSet)
-
-	open(tar1,'w').writelines(tarfile1list)
-	open(tar2,'w').writelines(tarfile2list)
+	fileFilter(in_file1,
+                   file1IDlist,
+                   open(out_1,"w"),
+                   passSet)
+	fileFilter(in_file2,
+                   file2IDlist,
+                   open(out_2,"w"),
+                   passSet)
 
 if __name__ == '__main__':
 	usage = "Usage: fastqUnpairedFilter.py in_seq_1 in_seq_2 out_seq_1 out_seq_2"
