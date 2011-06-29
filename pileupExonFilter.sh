79d1ab2a1344501b6dd976ef5b620e0ef9d1b376
ryota sugimoto <ryota.sugimoto@gmail.com>
2011-06-29 11:14:37 +0900

    added pileupExonFilter.sh

---
+++ pileupExonFilter.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+USAGE="Usage: pileupExonFilter.sh bedfile in_pileup out_pileup > report"
+if (( $# == 0 ))
+then
+    echo "${USAGE}" >&2
+    exit 1
+fi
+
+#check input file existece
+test -e "${1}" || { echo "${1} not found." >&2; exit 1; }
+test -e "${2}" || { echo "${2} not found." >&2; exit 1; }
+
+#check required commands
+function checkcommand()
+{
+    which "${1}" > /dev/null 2>&1 \
+      || { echo "Command ${1} not found." >&2; exit 1; }
+}
+checkcommand pileupExonFilter.py
+
+#main process
+pileupExonFilter.py "${1}" "${2}" > "${3}" \
+ || { echo "pileupExonFilter.py is failed" >&2; exit 1; }
+
+#report
+for chr in `awk '{ print $1 }' < "${1}.width" | sort -n -t r -k 2 | uniq`
+do
+    BEDWIDTH=$(grep "${chr}[[:space:]]" "${1}.width" | awk '{print $2}')
+    if [ $chr != "tortal" ]
+    then
+        PILEUPWIDTH=$(grep "${chr}[[:space:]]" "${3}" | wc -l)
+    else
+        PILEUPWIDTH=$(wc -l < "${3}")
+    fi
+    printf "%25s%15s%15s%15s\n" \
+    $chr \
+    $BEDWIDTH \
+    $PILEUPWIDTH \
+    $(echo "scale=6; $PILEUPWIDTH / $BEDWIDTH" | bc)
+done
