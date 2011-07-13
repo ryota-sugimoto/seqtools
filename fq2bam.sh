6d9ba4da0b03e6ff82209d08f4b7cc7514a98b7c
ryota sugimoto <ryota.sugimoto@gmail.com>
2011-07-13 13:32:07 +0900

    added options at fq2bam.sh

--- fq2bam.sh
+++ fq2bam.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
-USAGE="usage: fq2bam.sh in_seq_1 in_seq_2"
-if (( $# == 0 ))
-then
-    echo "${USAGE}" >&2 
-    exit 1
-fi
+USAGE="Usage: fq2bam.sh [options] in_seq_1 in_seq_2"
+
+#perse options
+OPTERR=0
+while getopts 'o:pr:t:l:' OPTION
+do
+    case $OPTION in
+    o) OUT="${OPTARG%/}/" ;;
+    r) REFERENCE=$OPTARG ;;
+    p) CREATEPILEUP=1 ;;
+    t) tOPT=$OPTARG ;;
+    l) lOPT=$OPTARG ;;
+    ?) { echo $USAGE >&2 ; exit 1; };;
+    esac
+done
+shift $(($OPTIND - 1))
 
 #check input file existece
-test -e "${1}" || { echo "${1} not found." >&2; exit 1; }
-test -e "${2}" || { echo "${2} not found." >&2; exit 1; }
+test -e "${1}" || { echo "File ${1} not found." >&2; exit 1; }
+test -e "${2}" || { echo "File ${2} not found." >&2; exit 1; }
 
 #check required commands
 function checkcommand()
 {
-    which "${1}" > /dev/null 2>&1 
+    which "${1}" > /dev/null 2>&1 \
       || { echo "Command ${1} not found." >&2; exit 1; }
 }
 checkcommand bwa
@@ -23,19 +33,23 @@ checkcommand fastqUnpairedFilter.py
 
 #create output directory
 FILE1DIR="$(dirname "${1}")/"
-OUT="${FILE1DIR}out/"
-mkdir "${OUT}" 2> /dev/null
+[ $OUT ] || OUT="${FILE1DIR}out/"
+mkdir -p "${OUT}" \
+  || { echo "Failed to create output directory" >&2; exit 1; }
 
 FILE1=$(basename "${1}")
 FILE2=$(basename "${2}")
 
 #path to the reference file
-REFERENCE=/usr/local/share/doc/hg19/hg19.fa
-test -e "${REFERENCE}" || { echo "${REFERENCE} not found." >&2; exit 1; }
+[ $REFERENCE ] || REFERENCE="/usr/local/share/doc/hg19/hg19.fa"
+test -e "${REFERENCE}" \
+  || { echo "Reference file ${REFERENCE} not found." >&2; exit 1; }
 
 #main processes
-fastq_quality_trimmer -t 20 -l 75 -i "${1}" -o "${OUT}${FILE1}.trimmed"
-fastq_quality_trimmer -t 20 -l 75 -i "${2}" -o "${OUT}${FILE2}.trimmed"
+[ $tOPT ] || tOPT=20
+[ $lOPT ] || lOPT=75
+fastq_quality_trimmer -t $tOPT -l $lOPT -i "${1}" -o "${OUT}${FILE1}.trimmed"
+fastq_quality_trimmer -t $tOPT -l $lOPT -i "${2}" -o "${OUT}${FILE2}.trimmed"
 
 fastqUnpairedFilter.py "${OUT}${FILE1}.trimmed" \
                                   "${OUT}${FILE2}.trimmed" \
@@ -68,10 +82,12 @@ samtools sort "${OUT}${BAMFN}" "${OUT}${SORTEDBAMFN}"
 
 samtools index "${OUT}${SORTEDBAMFN}.bam"
 
-PILEUPFN="${FILE1/_?_sequence.txt/_sequence}.pileup"
-samtools pileup -cf "${REFERENCE}" "${OUT}${SORTEDBAMFN}.bam" > "${OUT}${PILEUPFN}"
-
-awk '( $6 >= 20 ) && ( $8 >= 20 ){ print }' "${OUT}${PILEUPFN}" \
-                                          > "${OUT}filtered.${PILEUPFN}"
-
+if [ $CREATEPILEUP ]
+then
+  PILEUPFN="${FILE1/_?_sequence.txt/_sequence}.pileup"
+  samtools pileup -c -f "${REFERENCE}" "${OUT}${SORTEDBAMFN}.bam" \
+                      > "${OUT}${PILEUPFN}"
+  awk '( $6 >= 20 ) && ( $8 >= 20 ){ print }' "${OUT}${PILEUPFN}" \
+                                            > "${OUT}filtered.${PILEUPFN}"
+fi
 echo "fq2bam.sh: complete"
