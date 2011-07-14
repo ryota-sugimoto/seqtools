d771c24b54e23dec46106975913c353b3c9d4f21
ryota sugimoto <ryota.sugimoto@gmail.com>
2011-07-14 16:34:44 +0900

    fixed file name unmatching bugs

--- fq2bam.sh
+++ fq2bam.sh
@@ -10,7 +10,7 @@ do
     r) REFERENCE=$OPTARG ;;
     p) CREATEPILEUP=1 ;;
     q) DO_QUALITYTRIMM=1 ;;
-    u) DO_UNPAIRRFILTER=1 ;;
+    u) DO_UNPAIREDFILTER=1 ;;
     t) tOPT=$OPTARG ;;
     l) lOPT=$OPTARG ;;
     c) BWATHREAD=$OPTARG ;; 
@@ -51,53 +51,63 @@ test -e "${REFERENCE}" \
 #main processes
 [ $tOPT ] || tOPT=20
 [ $lOPT ] || lOPT=75
+
+INIT_FILE1=${1}
+INIT_FILE2=${2}
+
 if [ $DO_QUALITYTRIMM ]
 then
-  fastq_quality_trimmer -t $tOPT -l $lOPT -i "${1}" -o "${OUT}${FILE1}.trimmed"
-  fastq_quality_trimmer -t $tOPT -l $lOPT -i "${2}" -o "${OUT}${FILE2}.trimmed"
+  fastq_quality_trimmer -t $tOPT -l $lOPT -i "$INIT_FILE1" \
+                                          -o "${OUT}${FILE1}.trimmed"
+  fastq_quality_trimmer -t $tOPT -l $lOPT -i "$INIT_FILE2" \
+                                          -o "${OUT}${FILE2}.trimmed"
+  INIT_FILE1="${OUT}${FILE1}.trimmed"
+  INIT_FILE2="${OUT}${FILE2}.trimmed"
 fi
 
-if [ $DO_UNPAIRFILTER ]
+if [ $DO_UNPAIREDFILTER ]
 then
-  fastqUnpairedFilter.py "${OUT}${FILE1}.trimmed" \
-                                  "${OUT}${FILE2}.trimmed" \
-                                  "${OUT}${FILE1}.trimmed.filtered" \
-                                  "${OUT}${FILE2}.trimmed.filtered"
+  fastqUnpairedFilter.py "$INIT_FILE1" \
+                         "$INIT_FILE2" \
+                         "${OUT}${FILE1}.filtered" \
+                         "${OUT}${FILE2}.filtered"
+  INIT_FILE1="${OUT}${FILE1}.filtered"
+  INIT_FILE2="${OUT}${FILE2}.filtered"
 fi
 
 [ $BWATHREAD ] || BWATHREAD=4
 bwa aln -t $BWATHREAD "${REFERENCE}" \
-                      "${OUT}${FILE1}.trimmed.filtered" \
+                      "$INIT_FILE1" \
                     > "${OUT}${FILE1}.sai"
 
 bwa aln -t $BWATHREAD "${REFERENCE}" \
-                      "${OUT}${FILE2}.trimmed.filtered" \
+                      "$INIT_FILE2" \
                     > "${OUT}${FILE2}.sai"
 
-SAMFN="${FILE1/_?_sequence/_sequence}.sam"
+SAMFN=$(echo $FILE1 | sed 's/[12]\([^0-9][^0-9]*\)$/\1/').sam
 bwa sampe "${REFERENCE}" \
           "${OUT}${FILE1}.sai" \
           "${OUT}${FILE2}.sai" \
-          "${OUT}${FILE1}.trimmed.filtered" \
-          "${OUT}${FILE2}.trimmed.filtered" \
+          "$INIT_FILE1" \
+          "$INIT_FILE2" \
         > "${OUT}${SAMFN}"
 
-BAMFN="${FILE1/_?_sequence.txt/_sequence}.bam"
+BAMFN="${SAMFN%.sam}.bam"
 samtools import "${REFERENCE}.fai" \
                 "${OUT}${SAMFN}" \
                 "${OUT}${BAMFN}"
 
-SORTEDBAMFN="${BAMFN/.bam/.sorted}"
+SORTEDBAMFN="${BAMFN%.bam}.sorted"
 samtools sort "${OUT}${BAMFN}" "${OUT}${SORTEDBAMFN}"
 
 samtools index "${OUT}${SORTEDBAMFN}.bam"
 
 if [ $CREATEPILEUP ]
 then
-  PILEUPFN="${FILE1/_?_sequence.txt/_sequence}.pileup"
+  PILEUPFN=${SORTEDBAMFN%.sorted}.pileup
   samtools pileup -c -f "${REFERENCE}" "${OUT}${SORTEDBAMFN}.bam" \
                       > "${OUT}${PILEUPFN}"
   awk '( $6 >= 20 ) && ( $8 >= 20 ){ print }' "${OUT}${PILEUPFN}" \
-                                            > "${OUT}filtered.${PILEUPFN}"
+                           > "${OUT}${PILEUPFN%.pileup}.filtered.pileup"
 fi
 echo "fq2bam.sh: complete"
